7b46215ace6d8b04913a980385e1cf0b6a0cc34c
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-08-27 00:04:32 -0300

    Req 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -35,7 +35,7 @@ def scrape_updates(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    return Selector(html_content).css("a.next::attr(href)").get()
 
 
 # Requisito 4
