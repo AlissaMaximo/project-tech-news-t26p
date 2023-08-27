b98a2fdfaa440c5141c1b898cdbf3f6884513f1f
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-08-27 00:06:55 -0300

    Req 4 feito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -41,7 +41,20 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_news(html_content):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(html_content)
+    return {
+        "url": selector.css('link[rel="canonical"]::attr(href)').get(),
+        "title": selector.css(".entry-title::text").get().strip(),
+        "timestamp": selector.css("li.meta-date::text").get(),
+        "writer": selector.css("li.meta-author a::text").get(),
+        "reading_time": int(
+            selector.css("li.meta-reading-time::text").re_first(r"\d+")
+        ),
+        "summary": "".join(
+            selector.css(".entry-content > p:first-of-type *::text").getall()
+        ).strip(),
+        "category": selector.css(".label::text").get(),
+    }
 
 
 # Requisito 5
